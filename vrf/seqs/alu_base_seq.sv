1283481db3fe269685bed3888bb3ab36c4e6cf97
rndnckm <boombist45@gmail.com>
2025-12-04 05:26:40 +0300

    feat: add randomization in base_seq

--- vrf/seqs/alu_base_seq.sv
+++ vrf/seqs/alu_base_seq.sv
@@ -1,25 +1,27 @@
 class alu_base_seq extends uvm_sequence #(alu_seq_item);
 `uvm_object_utils(alu_base_seq)
 
+  rand int unsigned num_tests = 10;
+	
   function new(string name = "alu_base_seq");
     super.new(name);
   endfunction
   
+  alu_opcode_low_t op_selected;
+  
   task body();
 	alu_seq_item tr;
-	tr = alu_seq_item::type_id::create("tr");
-	initialize_test(tr);
-	start_item(tr);
-	finish_item(tr);
-  endtask: body
-  
-  // Helper tasks
-	  task initialize_test(alu_seq_item tr);
-		tr.operand_a <= 0;
-		tr.operand_b <= 0;
-		tr.mode <= 0;
-		tr.opcode <= 0;
-		tr.carry_in <= 0;
-	  endtask
+        repeat(num_tests) begin
+			assert(randomize(op_selected))
+                else `uvm_error("SEQ", "Enum randomization failed");
+            tr = alu_seq_item::type_id::create("tr");
+            assert(tr.randomize() with {
+                {mode, opcode} == op_selected; 
+            });
+            `uvm_info("ADD_SEQ", $sformatf("Generated %s op A=%h B=%h",  op_selected.name(), tr.operand_a, tr.operand_b), UVM_MEDIUM)
+            start_item(tr);
+            finish_item(tr);
+        end
+   endtask
 	  
 endclass
